8f7a161c8e93a511581a7fa271b2f910b599686b
Abhay2004Kumar <abs220502@gmail.com>
2025-05-21 17:42:10 +0530

    2nd commit

--- server.js
+++ server.js
@@ -6,4 +6,9 @@ const port = serverConfig.port;
 
 app.listen(port, () => {
   console.log(`Server running in ${serverConfig.env} mode on port ${port}`);
+  console.log("Attempting to connect to:", {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME
+  });
 });
